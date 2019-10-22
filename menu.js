ec9b8850da5ffe35d84f9208b60723f2ed1b314b
StoryTours <new.markets@gmx.net>
2019-10-22 19:02:53 +0200

    aktualisieren

--- menu.js
+++ menu.js
@@ -1,5 +1,1 @@
-document.write("Ich bin eine kleine Testdatei, bei der nun die Endung von txt in js "
-+ "umgetauft wurde. Mein Name spielt keine Rolle, nur meine Extension hat eine "
-+ "gewisse Bedeutung.<br>"
-+ "Weiterhin wird die in mir enthaltende Zeichenkette bzw. mein String nun in "
-+ "Klammern eingefasst, weil mein Inhalt sonst nicht ausgegeben werden kann.");
+document.write(" <a href=\"index.html\">Home</a> - <a href=\"page1.html\">Page1</a> ");
